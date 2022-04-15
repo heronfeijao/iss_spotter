f72c9b4ebf2faa1fa0d99cbe5ca91fbb528cc77d
Heron Feijao <heronfeijao@gmail.com>
2022-04-14 22:27:05 -0300

    promised-based files

---
+++ iss_promised.js
@@ -0,0 +1,30 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const lat = JSON.parse(body).latitude;
+  const lon = JSON.parse(body).longitude;
+  return request(`https://iss-pass.heroakuapp.com/json/?lat=${lat}&lon=${lon}`);
+};
+
+// * Input: None
+// * Returns: Promise for fly over data for users location
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
